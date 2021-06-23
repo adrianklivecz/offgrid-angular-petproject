54b26ed28960844b345177dcde0ebaebccedaa85
Adrian Clivet <adrian.clivet@agilenetworks.tech>
2021-06-23 11:46:12 +0300

    refactor: use structural directive

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -5,4 +5,6 @@ import { Component } from '@angular/core';
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.scss']
 })
-export class ProductListComponent {}
+export class ProductListComponent {
+  numbers = [0,1,2,3,4,5,6,7,8];
+}
