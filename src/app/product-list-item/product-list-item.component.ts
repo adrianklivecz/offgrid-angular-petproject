b592bdcc12dd711dcd8519207f398febc52b9a0c
Adrian Clivet <adrian.clivet@agilenetworks.tech>
2021-06-28 18:40:57 +0300

    feat: fetch server data for PLP cards

--- src/app/product-list-item/product-list-item.component.ts
+++ src/app/product-list-item/product-list-item.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../Product';
 
 @Component({
   selector: 'app-product-list-item',
   templateUrl: './product-list-item.component.html',
   styleUrls: ['./product-list-item.component.scss'],
 })
-export class ProductListItemComponent {}
+export class ProductListItemComponent implements OnInit {
+  products: Array<Product> = [];
+
+  ngOnInit() {
+    this.populateProductList();
+  }
+
+  populateProductList() {
+    fetch('http://localhost:8080/products', {
+      method: 'GET',
+    })
+      .then((res) => res.json())
+      .then((products) =>
+        products.map((product: Product) => this.products.push(product))
+      );
+  }
+}
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.scss']
+  styleUrls: ['./product-list.component.scss'],
 })
-export class ProductListComponent {
-  numbers = [0,1,2,3,4,5,6,7,8];
-}
+export class ProductListComponent {}
